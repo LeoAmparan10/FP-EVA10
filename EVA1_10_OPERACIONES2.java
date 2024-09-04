a19e4e7c22c69c6679541af4e772dd879acb3872
LeoAmparan10 <L24550381@chihuahua2.tecnm.mx>
2024-09-03 18:04:57 -0600

    Add files via upload

---
+++ EVA1_10_OPERACIONES2.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_10_operaciones2;
+
+/**
+ *
+ * @author chiri
+ */
+public class EVA1_10_OPERACIONES2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //CONCATENACION CADENAS:
+        //OPERADOR DE CONCATENACION + 
+        //Mismo simbolo con diferentes usos:
+        //sobrecarga de operadores 
+        String nombre = "Leonardo";
+        String apellido = "Amparan";
+        String nomComp;
+        
+        //CONCATENAR: unir cadenas de texto 
+        nomComp = nombre + " " + apellido;
+        System.out.println(nomComp);
+        
+        //Imprimir los datos:
+        System.out.println("Nombre de usuario: " + nombre);
+        System.out.println("Apellido del usuario: " + apellido);
+        System.out.println("Nombre completo del usuario " + nomComp);
+        
+        
+    
+    }
+    
+}
